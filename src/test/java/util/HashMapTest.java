ccd2f14366ac12de817d15da6f8019a19345b8b0
wangzhx <wangzhx@fxiaoke.com>
2020-04-24 15:53:56 +0800

    init

---
+++ src/test/java/util/HashMapTest.java
@@ -0,0 +1,14 @@
+package util;
+
+import java.util.HashMap;
+
+/**
+ * Created by wangzhx on 2020/3/16.
+ */
+public class HashMapTest {
+    public static void main(String[] args) {
+        HashMap map = new HashMap();
+        map.put(1,1);
+        System.out.println(map.size());
+    }
+}
